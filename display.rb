2df1d32c1354b5465b4af24c7196e651012a1d7b
Daniel Irvine <dirvine@8thlight.com>
2014-05-24 14:27:56 +0100

    updated display to show row and column numbers

--- display.rb
+++ display.rb
@@ -1,18 +1,19 @@
 class Display
 
+  HEADER_ROW = "   1  2  3"
   def initialize(io)
     @io = io
   end
 
   def display_board(board)
+    @io.puts HEADER_ROW
     (0..2).each do |row|
 
-      line = ""
+      line = (row + 1).to_s
       (0..2).each do |column|
         pos = row*3 + column
-        line << " "
+        line << "  "
         line << (board.played?(pos) ? board.mark_at(pos) : " ")
-        line << " "
       end
       @io.puts line
     end
--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -4,22 +4,28 @@ describe Display do
 
   it "displays an empty square for the empty board" do
     output = double()
-    expect(output).to receive(:puts).with("         ").exactly(3).times
+    expect(output).to receive(:puts).with("   1  2  3")
+    expect(output).to receive(:puts).with("1         ")
+    expect(output).to receive(:puts).with("2         ")
+    expect(output).to receive(:puts).with("3         ")
     Display.new(output).display_board(Board.start)
   end
 
   it "displays a mark in the correct position" do
     output = double()
-    expect(output).to receive(:puts).with(" X       ")
-    expect(output).to receive(:puts).with("         ").exactly(2).times
+    expect(output).to receive(:puts).with("   1  2  3")
+    expect(output).to receive(:puts).with("1  X      ")
+    expect(output).to receive(:puts).with("2         ")
+    expect(output).to receive(:puts).with("3         ")
     Display.new(output).display_board(Board.new "X--------")
   end
 
   it "displays a winning board" do
     output = double()
-    expect(output).to receive(:puts).with(" X       ")
-    expect(output).to receive(:puts).with(" O  X  O ")
-    expect(output).to receive(:puts).with("       X ")
+    expect(output).to receive(:puts).with("   1  2  3")
+    expect(output).to receive(:puts).with("1  X      ")
+    expect(output).to receive(:puts).with("2  O  X  O")
+    expect(output).to receive(:puts).with("3        X")
     Display.new(output).display_board(Board.new "X--OXO--X")
   end
 end
