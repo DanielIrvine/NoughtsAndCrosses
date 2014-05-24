c48afdbf49e15616778b2d6cd81a6f900dd3f523
Daniel Irvine <dirvine@8thlight.com>
2014-05-24 19:29:40 +0100

    display the board after each move

--- game.rb
+++ game.rb
@@ -1,23 +1,31 @@
 require './board'
+require './display'
 
 class Game
 
 	attr_reader :board
 
-	def initialize (player_x, player_o)
+	def initialize (player_x, player_o, io)
 		@board = Board.start
 		@player_x = player_x
 		@player_o = player_o
+    @io = io
 	end
 
-	def play!
-		@board = @player_x.make_move @board
-		@board = @player_o.make_move @board
+	def play_turn!
+    play_one!(@player_x)
+    play_one!(@player_o)
+    @board
 	end
+
+  def play_one!(player)
+    @board = player.make_move @board
+    @io.display_board(@board)
+  end
 	
 	def play_all!
 		while !@board.game_over?
-			play!
+			play_turn!
 		end
 	end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,10 +5,11 @@ describe "Game" do
 
 	let(:x) { Player.new("X") }
 	let(:o) { Player.new("O") }
+  let(:io) { double().as_null_object() }
 
 	describe "#new" do
 		it "creates an empty game board" do
-			game = Game.new(x, o)
+			game = Game.new(x, o, io)
 			game.board.available_spaces.length.should eq 9
 		end
 	end
@@ -16,22 +17,41 @@ describe "Game" do
 	describe "#make_play" do
 
 		it "plays both moves" do
-			board = Game.new(x, o).play!
+			board = Game.new(x, o, io).play_turn!
 			board.available_spaces.length.should eq 7
 		end
 
 		it "plays two different player marks" do
-			board = Game.new(x, o).play!
+			board = Game.new(x, o, io).play_turn!
 			board.played_spaces.map{ |sp| board.mark_at(sp) }.uniq.length.should eq 2
 		end
+
+    it "displays the board after the play is made" do
+      expect(io).to receive(:display_board) do |board| 
+        board.played_spaces.count == 1 
+      end
+      
+      expect(io).to receive(:display_board) do |board| 
+        board.played_spaces.count == 2 
+      end
+
+      Game.new(x, o, io).play_turn!
+    end
 	end
 
 	describe "#play_all" do
 
 		it "plays untils game over" do
-			game = Game.new(x, o)
+      game = Game.new(x, o, io)
 			game.play_all!
 			game.board.game_over?.should eq true
 		end
+
+    it "displays the initial board and after each move" do
+      expect(io).to receive(:display_board).with(anything()).exactly(8).times
+      game = Game.new(x, o, io)
+      game.play_all!
+    end
 	end
+
 end
