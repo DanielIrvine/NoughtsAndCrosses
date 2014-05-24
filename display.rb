4b3433d65dff03379cb4907ea80fd967ed89ec8b
Daniel Irvine <dirvine@8thlight.com>
2014-05-24 13:49:18 +0100

    finished display initial implementation

--- display.rb
+++ display.rb
@@ -5,6 +5,16 @@ class Display
   end
 
   def display_board(board)
-    3.times { @io.puts "         " }
+    (0..2).each do |row|
+
+      line = ""
+      (0..2).each do |column|
+        pos = row*3 + column
+        line << " "
+        line << (board.played?(pos) ? board.mark_at(pos) : " ")
+        line << " "
+      end
+      @io.puts line
+    end
   end
 end
--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -7,4 +7,19 @@ describe Display do
     expect(output).to receive(:puts).with("         ").exactly(3).times
     Display.new(output).display_board(Board.start)
   end
+
+  it "displays a mark in the correct position" do
+    output = double()
+    expect(output).to receive(:puts).with(" X       ")
+    expect(output).to receive(:puts).with("         ").exactly(2).times
+    Display.new(output).display_board(Board.new "X--------")
+  end
+
+  it "displays a winning board" do
+    output = double()
+    expect(output).to receive(:puts).with(" X       ")
+    expect(output).to receive(:puts).with(" O  X  O ")
+    expect(output).to receive(:puts).with("       X ")
+    Display.new(output).display_board(Board.new "X--OXO--X")
+  end
 end
