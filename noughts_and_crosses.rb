2edfa615be645af7824495dab27e41a2ed7419d9
Daniel Irvine <dirvine@8thlight.com>
2014-05-29 15:52:11 +0100

    changed Kernel to IO.console

--- noughts_and_crosses.rb
+++ noughts_and_crosses.rb
@@ -1,6 +1,7 @@
 require './human_computer_game'
 require './display'
+require 'io/console'
 
-display = Display.new(Kernel)
+display = Display.new(IO.console)
 
 HumanComputerGame.new(display, Game).play
