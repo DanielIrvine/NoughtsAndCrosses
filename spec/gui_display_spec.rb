2c86a96bcb2358f22b9ed9f89dca5fead4841874
Daniel Irvine <dirvine@8thlight.com>
2014-06-03 16:33:15 +0100

    fixed tests

--- spec/gui_display_spec.rb
+++ spec/gui_display_spec.rb
@@ -19,7 +19,7 @@ describe GUIDisplay do
 
   it "displays no squares for an empty board" do
     gui = double.as_null_object
-    expect(gui).to receive(:draw_text).with(anything, anything, anything)
+    expect(gui).to receive(:draw_square).with(anything, anything)
       .exactly(0).times
     
     display = GUIDisplay.new(gui)
@@ -28,21 +28,21 @@ describe GUIDisplay do
 
   it "displays an x when x is played" do
     gui = double.as_null_object
-    expect(gui).to receive(:draw_text).with('X', 0, 0)
+    expect(gui).to receive(:draw_square).with('X', 0)
     display = GUIDisplay.new(gui)
     display.display_board(Board.start.make_move(0, 'X'))
   end
 
   it "displays an x in the right place when played" do
     gui = double.as_null_object
-    expect(gui).to receive(:draw_text).with('X', 2, 1) 
+    expect(gui).to receive(:draw_square).with('X', 7) 
     display = GUIDisplay.new(gui)
     display.display_board(Board.start.make_move(7, 'X'))
  end
 
   it 'displays multiple squares' do
     gui = double.as_null_object
-    expect(gui).to receive(:draw_text).with(anything, anything, anything).exactly(3).times
+    expect(gui).to receive(:draw_square).with(anything, anything).exactly(3).times
     display = GUIDisplay.new(gui)
     display.display_board(Board.new 'XOX------')
   end
@@ -78,23 +78,16 @@ describe GUIDisplay do
     expect(display.next_move_available?).to eq false
   end
 
-  it "displays result text at correct location" do
-    gui = double.as_null_object
-    expect(gui).to receive(:draw_text).with(anything, 3, 0, 3)
-    display = GUIDisplay.new(gui)
-    display.display_result(Board.new 'XXXOO-O--')
-  end
-
   it 'displays draw text if board is drawn' do
     gui = double.as_null_object
-    expect(gui).to receive(:draw_text).with("It's a draw!", anything, anything, anything)
+    expect(gui).to receive(:draw_result).with("It's a draw!")
     display = GUIDisplay.new(gui)
     display.display_result(Board.new 'XOXOXOOXO')
   end
 
   it 'displays won text if board is won' do
     gui = double.as_null_object
-    expect(gui).to receive(:draw_text).with('X wins!', anything, anything, anything)
+    expect(gui).to receive(:draw_result).with('X wins!')
     display = GUIDisplay.new(gui)
     display.display_result(Board.new 'XXXOO-O--')
   end
