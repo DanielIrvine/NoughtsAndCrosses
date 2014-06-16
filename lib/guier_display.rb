d9d0f3b97ef7e862a2597bf7398ec116c680160e
Daniel Irvine <dirvine@8thlight.com>
2014-06-16 17:38:10 +0100

    beginning now starts the game properly

--- lib/guier_display.rb
+++ lib/guier_display.rb
@@ -1,3 +1,4 @@
+require 'game'
 require 'qt'
 require 'strings'
 
@@ -11,43 +12,54 @@ module NoughtsAndCrosses
       slots :begin
       attr_reader :game
       
+      X3 = 'x3'
+      X4 = 'x4'
+      XHuman = 'XHuman'
+      OHuman = 'OHuman'
+      XComputer = 'XComputer'
+      OComputer = 'OComputer'
+      PlayButton = 'PlayButton'
+
       def initialize
         super(nil)
+        @buttons = {}
         self.layout = @layout = Qt::VBoxLayout.new
         @layout.addLayout(@top_box_layout = Qt::HBoxLayout.new)
-        create_player_selection('X')
-        create_player_selection('O')
+        create_player_selection('X', XHuman, XComputer)
+        create_player_selection('O', OHuman, OComputer)
         create_board_size_selection
         create_play_button
       end
 
       def begin
-        @game = Game.new(true,
-                         true,
-                         false)
+        @game = Game.new(@buttons[XHuman].checked?,
+                         @buttons[OHuman].checked?,
+                         @buttons[X4].checked?)
       end
 
       def create_play_button
         @play_button = Qt::PushButton.new(translate(:play), self)
-        @play_button.object_name = 'play_button'
+        @play_button.object_name = PlayButton
         connect(@play_button, SIGNAL(:clicked), self, SLOT(:begin))
         @top_box_layout.addWidget(@play_button)
       end
 
       def create_board_size_selection
         create_buttons(translate(:board_size),
-                       [ { :id => :x3,
+                       [ { :id => X3,
+                           :text => :x3,
                            :toggle => true },
-                         { :id => :x4 } ])
+                         { :id => X4,
+                           :text => :x4} ])
       end
 
-      def create_player_selection(mark)
+      def create_player_selection(mark, human, computer)
         create_buttons( translate(:player_is, mark),
-                       [ { :id => :human_button,
-                            :prefix => mark,
+                       [ { :id => human,
+                           :text => :human_button,
                             :toggle => true },
-                          { :id => :computer_button,
-                            :prefix => mark } ])
+                          { :id => computer,
+                            :text => :computer_button } ])
       end
 
       def create_buttons(title, buttons)
@@ -55,13 +67,13 @@ module NoughtsAndCrosses
         layout.addWidget(Qt::Label.new(title))
         group = Qt::ButtonGroup.new
         buttons.each do |b|
-          label = translate(b[:id])
-          prefix = b[:prefix] || '' 
+          label = translate(b[:text])
           button = Qt::RadioButton.new(label, nil)
-          button.object_name = prefix + label 
+          button.object_name = b[:id] 
           button.toggle if b[:toggle]
           group.addButton(button)        
           layout.addWidget(button)
+          @buttons[b[:id]] = button
         end
         @top_box_layout.addLayout(layout)
       end
--- spec/guier_display_spec.rb
+++ spec/guier_display_spec.rb
@@ -26,17 +26,17 @@ module NoughtsAndCrosses
       end
 
       it 'can set two computer players' do
-        x_computer = find_widget(display, 'XComputer')
+        x_computer = find_widget(display, GuierDisplay::XComputer)
         x_computer.toggle
-        o_computer = find_widget(display, 'OComputer')
+        o_computer = find_widget(display, GuierDisplay::OComputer)
         o_computer.toggle
         expect(x_computer.checked?).to be true
         expect(o_computer.checked?).to be true
       end
 
       it 'is set to two human players by default' do
-        x_human = find_widget(display, 'XHuman')
-        o_human = find_widget(display, 'OHuman') 
+        x_human = find_widget(display, GuierDisplay::XHuman)
+        o_human = find_widget(display, GuierDisplay::OHuman) 
         expect(x_human.checked?).to be true
         expect(o_human.checked?).to be true
       end
@@ -53,15 +53,15 @@ module NoughtsAndCrosses
       end
 
       it 'is set to 3x3 by default' do
-        three = find_widget(display, translate(:x3))
-        four = find_widget(display, translate(:x4))
+        three = find_widget(display, GuierDisplay::X3)
+        four = find_widget(display, GuierDisplay::X4)
         expect(three.checked?).to be true
         expect(four.checked?).to be false
       end
 
       it 'can be set to 4x4' do
-        three = find_widget(display, translate(:x3))
-        four = find_widget(display, translate(:x4))
+        three = find_widget(display, GuierDisplay::X3)
+        four = find_widget(display, GuierDisplay::X4)
         four.toggle
         expect(three.checked?).to be false
         expect(four.checked?).to be true
@@ -72,12 +72,23 @@ module NoughtsAndCrosses
       end
 
       it 'begins game when play button is clicked' do
-        button = find_widget(display, 'play_button')
+        button = find_widget(display, GuierDisplay::PlayButton)
         emit button.clicked()
         expect(display.game).to_not be nil
         expect(display.game.x).to be_kind_of(HumanPlayer)
         expect(display.game.o).to be_kind_of(HumanPlayer)
         expect(display.game.board.size).to eq 3
+      end
+      
+      it 'can begin a computer vs computer 4x4 game' do
+        find_widget(display, GuierDisplay::X4).toggle
+        find_widget(display, GuierDisplay::XComputer).toggle
+        find_widget(display, GuierDisplay::OComputer).toggle
+        button = find_widget(display, GuierDisplay::PlayButton)
+        emit button.clicked()
+        expect(display.game.x).to be_kind_of(ComputerPlayer)
+        expect(display.game.o).to be_kind_of(ComputerPlayer)
+        expect(display.game.board.size).to eq 4
       end
 
     end
