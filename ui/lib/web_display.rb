9c539cfc91d92eb288b726048996186fff157b9d
Daniel Irvine <dirvine@8thlight.com>
2014-06-23 14:13:02 +0100

    showing an empty board of links

--- ui/lib/web_display.rb
+++ ui/lib/web_display.rb
@@ -1,7 +1,43 @@
-class WebDisplay
+require 'game'
 
-  def call(env)
-    ['200',
-     {'Content-Type' => 'text/html'} ]
+module NoughtsAndCrosses
+  module Web
+    class WebDisplay
+    
+      def call(env)
+        
+        page = ''
+        game = Game.new(true, true, false)
+        next_player = game.next_player
+        game.board.available_spaces.each do |sq|
+          game.set_next_human_move(sq)
+          board = next_player.make_move(game.board)
+          link = create_link(game, board_link(board))
+          page += "<a href=\"#{link}\"/>\n"
+        end
+        ['200',
+         {'Content-Type' => 'text/html'},
+         page ]
+      end
+    
+    
+      def board_link(board)
+        board_str = ''
+        
+        board.all_indexes.each do |sq|
+          board_str += board.mark_at(sq)
+        end
+        board_str
+      end
+
+      def create_link(game, board_str)
+        "/#{player_type(game.x)}/#{player_type(game.o)}/#{board_str}"
+      end
+
+      def player_type(player)
+        return player.class.name.split('::').last
+      end
+    end
   end
+
 end
--- ui/spec/web_display_spec.rb
+++ ui/spec/web_display_spec.rb
@@ -1,19 +1,44 @@
 require 'spec_helper'
 require 'web_display'
 
+RSpec::Matchers.define :have_link_to_path do |expected|
+  match do |actual|
+    /href="(.)*#{expected}"/.match(actual[2])
+  end
+end
+
 module NoughtsAndCrosses
   module Web
     describe WebDisplay do
 
-      it 'displays a web page' do
-        w = WebDisplay.new
+      let(:display) { WebDisplay.new }
 
-        result = w.call( { 'PATH_INFO' => '',
+      it 'displays a web page' do
+        result = display.call( { 'PATH_INFO' => '',
+                                 'QUERY_STRING' => '',
                          'REQUEST_METHOD' => 'GET' })
 
         expect(result[0]).to eq '200'
         expect(result[1]['Content-Type']).to eq 'text/html'
       end
+
+      it 'displays an empty board' do
+
+        result = display.call( { 'PATH_INFO' => 'H/H/---------',
+                                 'QUERY_STRING' => '',
+                                 'REQUEST_METHOD' => 'GET' })
+
+        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/X--------')
+        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/-X-------')
+        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/--X------')
+        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/---X-----')
+        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/----X----')
+        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/-----X---')
+        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/------X--')
+        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/-------X-')
+        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/--------X')
+      end
+
     end
   end
 end
