1a86af9aaa1c12b810e69516d9b7089d1e7e20b9
Daniel Irvine <dirvine@8thlight.com>
2014-06-23 15:14:19 +0100

    some quick refactorings

--- ui/lib/web_display.rb
+++ ui/lib/web_display.rb
@@ -4,15 +4,18 @@ require 'board'
 module NoughtsAndCrosses
   module Web
     class WebDisplay
+      
+      OK = '200'
+      ERROR = '400'
     
       def call(env)
         
         path = env['PATH_INFO']
-        return show_start if path.empty? 
+        return show('') if path.empty? 
 
         board_str = env['PATH_INFO'].split('/').last
         
-        return error('Invalid board') if !Board.valid_board_str(board_str)
+        return show('Invalid board', ERROR) if !Board.valid_board_str(board_str)
 
         page = ''
         game = Game.new(true, true, board: board_str)
@@ -21,25 +24,18 @@ module NoughtsAndCrosses
           game.set_next_human_move(sq)
           board = next_player.make_move(game.board)
           link = create_link(game, board.to_s)
-          page += "<a href=\"#{link}\"/>\n"
+          page += "<a href=\"#{link}\">-</a>\n"
         end
+
+        show(page)
+      end
+    
+      def show(page, status = OK)
         ['200',
          {'Content-Type' => 'text/html'},
          page ]
       end
-    
-      def show_start
-        ['200',
-         {'Content-Type' => 'text/html'},
-         '' ]
-      end
 
-      def error(str)
-        ['400',
-         {'Content-Type' => 'text/html'},
-         str ]
-      end
-    
       def create_link(game, board_str)
         "/#{player_type(game.x)}/#{player_type(game.o)}/#{board_str}"
       end
--- ui/spec/web_display_spec.rb
+++ ui/spec/web_display_spec.rb
@@ -11,35 +11,35 @@ module NoughtsAndCrosses
       let(:display) { WebDisplay.new }
 
       it 'displays a web page' do
-        result = display.call(get_request('')) 
-        expect(result[0]).to eq '200'
-        expect(result[1]['Content-Type']).to eq 'text/html'
+        response = display.call(get_request('')) 
+        expect(response[0]).to eq '200'
+        expect(response[1]['Content-Type']).to eq 'text/html'
       end
 
       it 'displays an empty board' do
-        result = display.call(get_request('HumanPlayer/HumanPlayer/---------'))
-        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/X--------')
-        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/-X-------')
-        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/--X------')
-        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/---X-----')
-        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/----X----')
-        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/-----X---')
-        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/------X--')
-        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/-------X-')
-        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/--------X')
+        response = display.call(get_request('HumanPlayer/HumanPlayer/---------'))
+        expect(response).to have_link_to_path('HumanPlayer/HumanPlayer/X--------')
+        expect(response).to have_link_to_path('HumanPlayer/HumanPlayer/-X-------')
+        expect(response).to have_link_to_path('HumanPlayer/HumanPlayer/--X------')
+        expect(response).to have_link_to_path('HumanPlayer/HumanPlayer/---X-----')
+        expect(response).to have_link_to_path('HumanPlayer/HumanPlayer/----X----')
+        expect(response).to have_link_to_path('HumanPlayer/HumanPlayer/-----X---')
+        expect(response).to have_link_to_path('HumanPlayer/HumanPlayer/------X--')
+        expect(response).to have_link_to_path('HumanPlayer/HumanPlayer/-------X-')
+        expect(response).to have_link_to_path('HumanPlayer/HumanPlayer/--------X')
       end
 
       it 'displays a board after one move' do
-        result = display.call(get_request('HumanPlayer/HumanPlayer/X--------'))
+        response = display.call(get_request('HumanPlayer/HumanPlayer/X--------'))
 
-        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/XO-------')
-        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/X-O------')
-        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/X--O-----')
-        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/X---O----')
-        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/X----O---')
-        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/X-----O--')
-        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/X------O-')
-        expect(result).to have_link_to_path('HumanPlayer/HumanPlayer/X-------O')
+        expect(response).to have_link_to_path('HumanPlayer/HumanPlayer/XO-------')
+        expect(response).to have_link_to_path('HumanPlayer/HumanPlayer/X-O------')
+        expect(response).to have_link_to_path('HumanPlayer/HumanPlayer/X--O-----')
+        expect(response).to have_link_to_path('HumanPlayer/HumanPlayer/X---O----')
+        expect(response).to have_link_to_path('HumanPlayer/HumanPlayer/X----O---')
+        expect(response).to have_link_to_path('HumanPlayer/HumanPlayer/X-----O--')
+        expect(response).to have_link_to_path('HumanPlayer/HumanPlayer/X------O-')
+        expect(response).to have_link_to_path('HumanPlayer/HumanPlayer/X-------O')
       end
 
       it 'displays error with invalid board size' do
@@ -51,6 +51,7 @@ module NoughtsAndCrosses
         response = display.call(get_request('H/H/ABCDEFGHI'))
         expect(response).to include('Invalid board')
       end
+
     end
   end
 end
