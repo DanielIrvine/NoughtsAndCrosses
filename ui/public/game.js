61937186a3efe0d4054301eda62874544b283222
Daniel Irvine <dirvine@8thlight.com>
2014-07-09 16:29:55 +0100

    setting link on board squares

--- ui/public/game.js
+++ ui/public/game.js
@@ -1,27 +1,61 @@
 function parse(json)
 {
-  $.ajax({url:"/best_move?board=-"});
+}
+
+function parse(json, callback)
+{
+  convertBoard(json).forEach(function(sq, i) {
+    var sqid = "#sq-" + i;
+    var elem = $(sqid).find('a');
+    elem.empty();
+    if (sq.link) { 
+      elem.on('click', function(){make_move(i, json.board)}); 
+    }
+    else {
+      elem.off('click');
+    }
+    elem.append(sq.text);
+  });
+  // TODO: set status text
+  if(json.next_move == "computer")
+  {
+    $.ajax({url: "/best_move?board=-",
+            success: callback });
+  }
+}
+
+function convertToLink(sq, board)
+{
+  return "make_move(" + sq + "'" + board + "');";
+}
+
+function make_move(sq, board)
+{
+  $.ajax({url: "/make_move?sq=" + sq + "&board=" + board,
+          success: function(response) 
+  {
+    parse(response, function(response) {
+      make_move(response.best_move, response.board);
+    });
+  } });
 }
 
 function convertBoard(json)
 {
   var createLinks = json.next_move != "computer";
-  return json.board.split('').map(function(sq, i) {
-    return createSquare(sq, i, json.board, createLinks);
+  return json.board.split('').map(function(sq) {
+    return createSquare(sq, createLinks);
   });
 }
 
-function createSquare(sq, i, board, createLinks)
+function createSquare(sq, createLinks)
 {
   if(sq == '-') {
-    return createLinks ? createLink(i, board) : "";
+    return createLinks ? {text: "", link: true} : {text: ""};
   }
   else {
-    return sq;
+    return {text: sq};
   }
 }
 
-function createLink(sq, board)
-{
-  return "make_move?sq=" + sq + "&board=" + board;
-}
+
--- ui/spec/js/game_spec.js
+++ ui/spec/js/game_spec.js
@@ -1,36 +1,79 @@
 describe("game", function(){
 
-  it("converts a string to an array of HTML segments", function() {
+  it("converts an empty square to a link", function() {
     expect(convertBoard({board:"-"})).toEqual(
-      ["make_move?sq=0&board=-"])
+      [{text:"", link:true}])
   });
 
   it("converts all squares", function() {
     expect(convertBoard({board:"---"})).toEqual(
-      ["make_move?sq=0&board=---",
-      "make_move?sq=1&board=---",
-      "make_move?sq=2&board=---"])
+      [{text:"", link:true},
+      {text:"", link:true},
+      {text:"", link:true}])
   });
 
   it("displays X", function() {
-    expect(convertBoard({board:"X"})).toEqual(["X"])
+    expect(convertBoard({board:"X"})).toEqual([{text:"X"}])
   });
 
   it("displays O", function() {
-    expect(convertBoard({board: "O"})).toEqual(["O"]);
+    expect(convertBoard({board: "O"})).toEqual([{text:"O"}]);
   });
 
   it("does not display links for a computer move", function() {
-    expect(convertBoard({board: "-", next_move: "computer"})).toEqual([""]);
+    expect(convertBoard({board: "-", next_move: "computer"})).toEqual([{text:""}]);
   });
 
   it("displays already played squares for a computer move", function() {
-    expect(convertBoard({board: "X", next_move: "computer"})).toEqual(["X"]);
+    expect(convertBoard({board: "X", next_move: "computer"})).toEqual([{text:"X"}]);
   });
 
-  it("should make an AJAX request for a computer url", function() {
-    spyOn($, "ajax");
-    parse({board:"-", next_move: "computer"});
+  it("makes an AJAX request for a computer url", function() {
+    spyOn($, "ajax").andCallFake(function(opts){
+      opts.success();
+    });
+    var callback = jasmine.createSpy();
+    parse({board:"-", next_move: "computer"}, callback);
     expect($.ajax.mostRecentCall.args[0]["url"]).toEqual("/best_move?board=-");
+    expect(callback).toHaveBeenCalled();
+  });
+
+  it("makes an AJAX request for a human move", function() {
+    spyOn($, "ajax");
+    parse({board:"-"});
+    expect($.ajax.callCount).toEqual(0);
+  });
+
+  xit("makes an AJAX request when a square is clicked", function() {
+    spyOn($, "ajax");
+    make_move(1, "---");
+    expect($.ajax.mostRecentCall.args[0]["url"]).toEqual("/make_move?sq=1&board=---");
+  });
+
+  it("sets square content when parsing json", function() {
+    setFixtures('<div id="sq-0"><a/></div>'+
+                '<div id="sq-1"><a/></div>'+
+                '<div id="sq-2"><a/></div>');
+    parse({board:"X-O"});
+    expect($('#sq-0')).toHaveText("X");
+    expect($('#sq-1').find('a')).toBeEmpty();
+    expect($('#sq-2')).toHaveText("O");
+  });
+
+  it("sets square link when parsing json", function() {
+    spyOn($, "ajax");
+    setFixtures('<div id="sq-0"><a/></div>');
+    parse({board:"-"});
+    $('#sq-0').find('a').trigger('click');
+    expect($.ajax.callCount).toEqual(1);
+  });
+
+  it("unsets click handler", function() {
+    spyOn($, "ajax");
+    setFixtures('<div id="sq-0"><a/></div>');
+    parse({board:"-"});
+    parse({board:"X"});
+    $('#sq-0').find('a').trigger('click');
+    expect($.ajax.callCount).toEqual(0);
   });
 });
