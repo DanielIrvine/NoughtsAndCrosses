02113994a8d4777ae7b5934247e50243fd7346f4
Daniel Irvine <dirvine@8thlight.com>
2014-07-11 14:28:39 +0100

    fixed up references to that my VIM shortcuts work. added module pattern to js

--- ui/public/game.js
+++ ui/public/game.js
@@ -1,54 +1,67 @@
-function parse(json)
-{
-  convertBoard(json).forEach(function(sq, i) {
-    set_square_content(sq, i, json);
-  });
+var NoughtsAndCrosses = NoughtsAndCrosses || {};
 
-  if (json.gameOver) {
-  if(json.next_move == "computer")
+NoughtsAndCrosses.Game = function() {
+
+  var parse = function(json)
   {
-    make_move('', json.board, json.x, json.o)
+    convertBoard(json).forEach(function(sq, i) {
+      set_square_content(sq, i, json);
+    });
+
+    if(json.next_move == "computer")
+    {
+      make_move('', json.board, json.x, json.o)
+    }
+    set_status_text(json.status_text);
   }
-  set_status_text(json.status_text);
-}
 
-function set_square_content(sq, i, json)
-{
-  var sqid = "#sq-" + i;
-  var elem = $(sqid).find('a');
-  elem.empty();
-  elem.off('click');
-  if (should_display_link(sq, json)) {
-    elem.on('click', function(){make_move(i, json.board, json.x, json.o)}); 
-  }
-  elem.append(sq.text);
-}
-
-function should_display_link(sq, json) 
-{
-  return sq.link && !json.finished && json.next_move !== "computer";
-}
-
-function set_status_text(text)
-{
-  $('#status').empty();
-  $('#status').append(text);
-}
-
-function make_move(sq, board, x, o, parse_callback)
-{
-  if (typeof parse_callback === "undefined") 
+  var set_square_content = function(sq, i, json)
   {
-    parse_callback = function(result) { parse(result); };
+    var sqid = "#sq-" + i;
+    var elem = $(sqid).find('a');
+    elem.empty();
+    elem.off('click');
+    if (should_display_link(sq, json)) {
+      elem.on('click', function(){make_move(i, json.board, json.x, json.o)}); 
+    }
+    elem.append(sq.text);
   }
-  $.ajax({url: "/make_move?sq=" + sq + "&board=" + board + "&x=" + x + "&o=" + o,
-          success: parse_callback});
-}
 
-function convertBoard(json)
-{
-  return json.board.split('').map(function(sq) {
-    return sq == '-' ? {link: true} : {text: sq};
-  });
-}
+  var should_display_link = function(sq, json) 
+  {
+    return sq.link && !json.finished && json.next_move !== "computer";
+  }
 
+  var set_status_text = function(text)
+  {
+    $('#status').empty();
+    $('#status').append(text);
+  }
+
+  var make_move = function(sq, board, x, o, parse_callback)
+  {
+    if (typeof parse_callback === "undefined") 
+    {
+      parse_callback = function(result) { parse(result); };
+    }
+    $.ajax({url: "/make_move?sq=" + sq + "&board=" + board + "&x=" + x + "&o=" + o,
+      success: parse_callback});
+  }
+
+  var convertBoard = function(json)
+  {
+    return json.board.split('').map(function(sq) {
+      return sq == '-' ? {link: true} : {text: sq};
+    });
+  }
+
+  var oPublic =
+  {
+    convertBoard: convertBoard,
+    parse: parse,
+    make_move: make_move
+  };
+
+  return oPublic;
+
+}();
--- ui/spec/js/game_spec.js
+++ ui/spec/js/game_spec.js
@@ -1,23 +1,23 @@
 describe("game", function(){
 
   it("converts an empty square to a link", function() {
-    expect(convertBoard({board:"-"})).toEqual(
-      [{link:true}])
-  );
+    expect(NoughtsAndCrosses.Game.convertBoard({board:"-"})).toEqual(
+      [{link:true}]);
+  });
 
   it("converts all squares", function() {
-    expect(convertBoard({board:"---"})).toEqual(
+    expect(NoughtsAndCrosses.Game.convertBoard({board:"---"})).toEqual(
       [{link:true},
       {link:true},
-      {link:true}])
+      {link:true}]);
   });
 
   it("displays X", function() {
-    expect(convertBoard({board:"X"})).toEqual([{text:"X"}])
+    expect(NoughtsAndCrosses.Game.convertBoard({board:"X"})).toEqual([{text:"X"}]);
   });
 
   it("displays O", function() {
-    expect(convertBoard({board: "O"})).toEqual([{text:"O"}]);
+    expect(NoughtsAndCrosses.Game.convertBoard({board: "O"})).toEqual([{text:"O"}]);
   });
 
   it("makes an AJAX request for a computer player", function() {
@@ -25,19 +25,19 @@ describe("game", function(){
     spyOn($, "ajax").andCallFake(function(opts){
       if (!bestMoveCall) bestMoveCall = opts.url;
     });
-    parse({board:"-", next_move: "computer", x: "ComputerPlayer", o: "HumanPlayer" });
+    NoughtsAndCrosses.Game.parse({board:"-", next_move: "computer", x: "ComputerPlayer", o: "HumanPlayer" });
     expect(bestMoveCall).toEqual("/make_move?sq=&board=-&x=ComputerPlayer&o=HumanPlayer");
   });
 
   it("does not make an AJAX request for a human player", function() {
     spyOn($, "ajax");
-    parse({board:"-"});
+    NoughtsAndCrosses.Game.parse({board:"-"});
     expect($.ajax.callCount).toEqual(0);
   });
 
   it("makes an AJAX request when a square is clicked", function() {
     spyOn($, "ajax");
-    make_move(1, "---", "HumanPlayer", "HumanPlayer");
+    NoughtsAndCrosses.Game.make_move(1, "---", "HumanPlayer", "HumanPlayer");
     expect($.ajax.mostRecentCall.args[0]["url"]).toEqual("/make_move?sq=1&board=---&x=HumanPlayer&o=HumanPlayer");
   });
 
@@ -46,7 +46,7 @@ describe("game", function(){
       opts.success();
     });
     var callback = jasmine.createSpy();
-    make_move(0, "---", "HumanPlayer", "HumanPlayer", callback);
+    NoughtsAndCrosses.Game.make_move(0, "---", "HumanPlayer", "HumanPlayer", callback);
     expect(callback).toHaveBeenCalled();
   });
 
@@ -54,7 +54,7 @@ describe("game", function(){
     setFixtures('<div id="sq-0"><a/></div>'+
                 '<div id="sq-1"><a/></div>'+
                 '<div id="sq-2"><a/></div>');
-    parse({board:"X-O"});
+    NoughtsAndCrosses.Game.parse({board:"X-O"});
     expect($('#sq-0')).toHaveText("X");
     expect($('#sq-1').find('a')).toBeEmpty();
     expect($('#sq-2')).toHaveText("O");
@@ -63,7 +63,7 @@ describe("game", function(){
   it("sets square link when parsing json", function() {
     spyOn($, "ajax");
     setFixtures('<div id="sq-0"><a/></div>');
-    parse({board:"-"});
+    NoughtsAndCrosses.Game.parse({board:"-"});
     $('#sq-0').find('a').trigger('click');
     expect($.ajax.callCount).toEqual(1);
   });
@@ -71,22 +71,22 @@ describe("game", function(){
   it("unsets click handler", function() {
     spyOn($, "ajax");
     setFixtures('<div id="sq-0"><a/></div>');
-    parse({board:"-"});
-    parse({board:"X"});
+    NoughtsAndCrosses.Game.parse({board:"-"});
+    NoughtsAndCrosses.Game.parse({board:"X"});
     $('#sq-0').find('a').trigger('click');
     expect($.ajax.callCount).toEqual(0);
   });
 
   it("displays status text", function() {
     setFixtures('<div id="status" />');
-    parse({board:"XXOOOXXOX", status_text: "It's a draw!"});
+    NoughtsAndCrosses.Game.parse({board:"XXOOOXXOX", status_text: "It's a draw!"});
     expect($('#status')).toHaveText("It's a draw!");
   });
 
   it("has no links if game is finished", function() {
     spyOn($, "ajax");
     setFixtures('<div id="sq-0"><a/></div>');
-    parse({board:"-", finished: true});
+    NoughtsAndCrosses.Game.parse({board:"-", finished: true});
     $('#sq-0').find('a').trigger('click');
     expect($.ajax.callCount).toEqual(0);
   });
