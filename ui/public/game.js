7a3790e84cc35602bce61f3abb8eb1f89d0a4115
Daniel <daniel@conciselycrafted.com>
2014-07-22 16:27:18 +0100

    made changes after running jslint. one error remains

--- ui/public/game.js
+++ ui/public/game.js
@@ -1,97 +1,92 @@
+/*jslint indent: 2, browser: true */
+/*global $ */
 var NoughtsAndCrosses = {};
 
-NoughtsAndCrosses.Game = function () {
+NoughtsAndCrosses.Game = (function () {
 
-  var url = '';
+  "use strict";
 
-  var parse = function (json){
-    var board = convertBoard(json);
-    var i;
-    for(i = 0; i < board.length; ++i) { 
-      setSquareContent(board[i], i, json);
-    }
+  var url = '',
 
-    if(shouldPlayNextComputerMove(json))
-    {
-      setTimeout(function(){
-        makeMove('', json.board, json.x, json.o);
-      }, 1000);
-    }
-    setStatusText(json.status_text);
-  };
+    urlRoot = function () {
+      var rootLocation = url.lastIndexOf('/');
+      return rootLocation === -1 ? '' : url.slice(0, rootLocation);
+    },
 
-  var setSquareContent = function(sq, i, json)
-  {
-    var sqid = "#sq-" + i;
-    var elem = $(sqid).find('a');
-    elem.empty();
-    elem.off('click');
-    if (shouldDisplayLink(sq, json)) {
-      elem.on('click', function(){makeMove(i, json.board, json.x, json.o);}); 
-    }
-    elem.append(sq.text);
-  };
+    makeMove = function (sq, board, x, o) {
+      var newUrl = urlRoot() + "/make_move?sq=" + sq + "&board=" + board + "&x=" + x + "&o=" + o;
+      $.ajax({url: newUrl,
+        success: parse});
+    },
 
-  var shouldPlayNextComputerMove = function(json)
-  {
-    return !json.finished && json.next_move == "computer";
-  };
-  
-  var shouldDisplayLink = function(sq, json) 
-  {
-    return sq.link && !json.finished && json.next_move !== "computer";
-  };
+    shouldPlayNextComputerMove = function (json) {
+      return !json.finished && json.next_move === "computer";
+    },
 
-  var setStatusText = function(text)
-  {
-    $('#status').empty();
-    $('#status').append(text);
-  };
+    shouldDisplayLink = function (sq, json) {
+      return sq.link && !json.finished && json.next_move !== "computer";
+    },
 
-  var makeMove = function(sq, board, x, o)
-  {
-    var newUrl = urlRoot() + "/make_move?sq=" + sq + "&board=" + board + "&x=" + x + "&o=" + o;
-    $.ajax({url: newUrl,
-      success: parse});
-  };
+    setSquareContent = function (sq, i, json) {
+      var sqid = "#sq-" + i,
+        elem = $(sqid).find('a');
+      elem.empty();
+      elem.off('click');
+      if (shouldDisplayLink(sq, json)) {
+        elem.on('click', function () {makeMove(i, json.board, json.x, json.o); });
+      }
+      elem.append(sq.text);
+    },
 
-  var convertBoard = function(json)
-  {
-    var board = [];
-    for(var i = 0; i < json.board.length; ++i)
-    {
-      var sq = json.board[i];
-      board[i] = sq == '-' ? {link: true} : {text: sq};
-    }
-    return board;
-  };
+    setStatusText = function (text) {
+      $('#status').empty();
+      $('#status').append(text);
+    },
 
-  var start = function(root_url)
-  {
-    url = root_url;
-    var argsLocation = url.indexOf('?');
-    var args = url.slice(argsLocation + 1);
-    var newUrl = urlRoot() + "/get_board?" + args;
-    $.ajax({url: newUrl, success: parse});
-  };
+    convertBoard = function (json) {
+      var i = 0, board = [], sq;
+      for (i = 0; i < json.board.length; i += 1) {
+        sq = json.board[i];
+        board[i] = sq === '-' ? {link: true} : {text: sq};
+      }
+      return board;
+    },
 
-  var urlRoot = function() {
-    var rootLocation = url.lastIndexOf('/');
-    return rootLocation === -1 ? '' : url.slice(0, rootLocation);
-  };
+    parse = function (json) {
+      var board = convertBoard(json), i;
+      for (i = 0; i < board.length; i += 1) {
+        setSquareContent(board[i], i, json);
+      }
+
+      if (shouldPlayNextComputerMove(json)) {
+        setTimeout(function () {
+          makeMove('', json.board, json.x, json.o);
+        }, 1000);
+      }
+      setStatusText(json.status_text);
+    },
+
+    start = function (root_url) {
+      url = root_url;
+      var argsLocation = url.indexOf('?'),
+        args = url.slice(argsLocation + 1),
+        newUrl = urlRoot() + "/get_board?" + args;
+      $.ajax({url: newUrl, success: parse});
+    };
 
   return {
     convertBoard: convertBoard,
     parse: parse,
     makeMove: makeMove,
     start: start,
-    set_url: function(new_url) {
+    set_url: function (new_url) {
       url = new_url;
     }
   };
 
-}();
+}());
 
-$(document).ready(function() {
+$(document).ready(function () {
+  "use strict";
   NoughtsAndCrosses.Game.start(window.location.href);
 });
