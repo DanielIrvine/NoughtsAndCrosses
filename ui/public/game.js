4a881871b7ae423e295dc6a76a371a3053483574
Daniel Irvine <dirvine@8thlight.com>
2014-08-07 15:59:35 +0100

    separated declarations and assignments

--- ui/public/game.js
+++ ui/public/game.js
@@ -6,61 +6,69 @@ NoughtsAndCrosses.Game = (function () {
 
   "use strict";
 
-  var url = '',
-
-    urlRoot = function () {
-      var rootLocation = url.lastIndexOf('/');
-      return rootLocation === -1 ? '' : url.slice(0, rootLocation);
-    },
-
+  var convertBoard,
+    makeMove,
     parse,
+    setSquareContent,
+    setStatusText,
+    shouldDisplayLink,
+    shouldPlayNextComputerMove,
+    start,
+    url = '',
+    urlRoot;
 
-    makeMove = function (sq, board, x, o) {
-      var newUrl = urlRoot() + "/make_move?sq=" + sq + "&board=" + board + "&x=" + x + "&o=" + o;
-      $.ajax({url: newUrl,
-        success: parse});
-    },
 
-    shouldPlayNextComputerMove = function (json) {
-      return !json.finished && json.next_move === "computer";
-    },
+  urlRoot = function () {
+    var rootLocation = url.lastIndexOf('/');
+    return rootLocation === -1 ? '' : url.slice(0, rootLocation);
+  };
 
-    shouldDisplayLink = function (sq, json) {
-      return sq.link && !json.finished && json.next_move !== "computer";
-    },
+  makeMove = function (sq, board, x, o) {
+    var newUrl = urlRoot() + "/make_move?sq=" + sq + "&board=" + board + "&x=" + x + "&o=" + o;
+    $.ajax({url: newUrl,
+      success: parse});
+  };
 
-    setSquareContent = function (sq, i, json) {
-      var sqid = "#sq-" + i,
+  shouldPlayNextComputerMove = function (json) {
+    return !json.finished && json.next_move === "computer";
+  };
+
+  shouldDisplayLink = function (sq, json) {
+    return sq.link && !json.finished && json.next_move !== "computer";
+  };
+
+  setSquareContent = function (sq, i, json) {
+    var sqid = "#sq-" + i,
         elem = $(sqid).find('a');
-      elem.empty();
-      elem.off('click');
-      if (shouldDisplayLink(sq, json)) {
-        elem.on('click', function () {makeMove(i, json.board, json.x, json.o); });
-      }
-      elem.append(sq.text);
-    },
+    elem.empty();
+    elem.off('click');
+    if (shouldDisplayLink(sq, json)) {
+      elem.on('click', function () {makeMove(i, json.board, json.x, json.o); });
+    }
+    elem.append(sq.text);
+  };
 
-    setStatusText = function (text) {
-      $('#status').empty();
-      $('#status').append(text);
-    },
+  setStatusText = function (text) {
+    $('#status').empty();
+    $('#status').append(text);
+  };
 
-    convertBoard = function (json) {
-      var i = 0, board = [], sq;
-      for (i = 0; i < json.board.length; i += 1) {
-        sq = json.board[i];
-        board[i] = sq === '-' ? {link: true} : {text: sq};
-      }
-      return board;
-    },
+  convertBoard = function (json) {
+    var i = 0, board = [], sq;
+    for (i = 0; i < json.board.length; i += 1) {
+      sq = json.board[i];
+      board[i] = sq === '-' ? {link: true} : {text: sq};
+    }
+    return board;
+  };
 
-    start = function (root_url) {
-      url = root_url;
-      var argsLocation = url.indexOf('?'),
+  start = function (root_url) {
+    url = root_url;
+    var argsLocation = url.indexOf('?'),
         args = url.slice(argsLocation + 1),
         newUrl = urlRoot() + "/get_board?" + args;
-      $.ajax({url: newUrl, success: parse});
-    };
+    $.ajax({url: newUrl, success: parse});
+  };
 
   parse = function (json) {
     var board = convertBoard(json), i;
@@ -74,9 +82,9 @@ NoughtsAndCrosses.Game = (function () {
       }, 1000);
     }
     setStatusText(json.status_text);
-  };
+    };
 
-  return {
+    return {
     convertBoard: convertBoard,
     parse: parse,
     makeMove: makeMove,
